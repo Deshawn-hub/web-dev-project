3c78b6fd64467c574c6a3cb7aea386280092b752
cckhiani <cckhiani@gmail.com>
2024-12-14 16:25:42 -0500

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
+     <link rel="stylesheet" href="login.css"></head>
 <body>
     <form action="index.php" method="post">
         username<br>
